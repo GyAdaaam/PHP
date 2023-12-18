aff0295260a9e33e5dcb42a6a79de70ee35a6a8b
Gyányi Ádám <144169768+GyAdaaam@users.noreply.github.com>
2023-12-18 21:55:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ if (isset($_POST["register"])) {
     if (empty($username) || empty($email) || empty($password)) {
         echo '<script>alert("A mező nem lehet üres")</script>';
     } else {
+        
         //Felhasználó vagy e-mail cím ellenőrzése, hogy már létezik-e az adatbázisban.
         $stmt = $dbh->prepare("SELECT * FROM login WHERE username = :username OR email = :email");
         $stmt->bindParam(':username', $username, PDO::PARAM_STR);
@@ -25,6 +26,7 @@ if (isset($_POST["register"])) {
                 echo '<script>alert("Ez az email már fel lett használva regisztráció során!")</script>';
             }
         } else {
+            
             //A jelszó hashelése a regisztrációnál.
             $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
@@ -52,10 +54,11 @@ if (isset($_POST["login"])) {
         $loginRow = $stmt->fetch(PDO::FETCH_ASSOC);
         if ($stmt->rowCount() == 1) {
             $hashedPassword = $loginRow['password'];
+            
             //Ellenőrzés,  a hash-elt jelszót a bejelentkezésnél.
             if (password_verify($password, $hashedPassword)) {
                 $_SESSION['username'] = $username;
-                header('Location: login.php'); // Vagy bármilyen más oldal, ahova az autentikált felhasználót át szeretnénk irányítani.
+                header('Location: login.php');
                 exit();
             } else {
                 echo '<script>alert("Hibás felhasználónév vagy jelszó")</script>';
